731c7472a3ab6b68c0ef30911ebe6020d3b32bb9
Alisson Lauffer <alissonvitortc@gmail.com>
2020-01-20 16:52:11 -0300

    commands: added sed/regex

--- korone/handlers/commands.py
+++ korone/handlers/commands.py
@@ -15,6 +15,7 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import html
+import regex
 import base64
 import string
 import random
@@ -289,6 +290,42 @@ async def gencode(c: Client, m: Message):
 
     codes_str = "\n".join(codes)
     await m.reply_text(f"<code>{codes_str}</code>")
+
+
+@Client.on_message(filters.regex(r"^s/(.+)?/(.+)?(/.+)?") & filters.reply)
+async def sed(c: Client, m: Message):
+    exp = regex.split(r"(?<![^\\]\\)/", m.text)
+    pattern = exp[1]
+    replace_with = exp[2].replace(r"\/", "/")
+    flags = exp[3] if len(exp) > 3 else ""
+
+    count = 1
+    rflags = 0
+
+    if "g" in flags:
+        count = 0
+    if "i" in flags and "s" in flags:
+        rflags = regex.I | regex.S
+    elif "i" in flags:
+        rflags = regex.I
+    elif "s" in flags:
+        rflags = regex.S
+
+    text = m.reply_to_message.text or m.reply_to_message.caption
+
+    if not text:
+        return
+
+    try:
+        res = regex.sub(
+            pattern, replace_with, text, count=count, flags=rflags, timeout=1
+        )
+    except TimeoutError:
+        await m.reply_text("Opa, seu padrão regex durou muito tempo.")
+    except regex.error as e:
+        await m.reply_text(str(e))
+    else:
+        await m.reply_to_message.reply_text(f"{html.escape(res)}")
 
 
 @Client.on_message(filters.cmd(command="spacex", action="Informações sobre a SpaceX."))
