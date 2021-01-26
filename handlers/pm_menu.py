a6f945dc03c8f5721f870ee5f8c018f721d9b4c1
Hitalo <hitalo331@outlook.com>
2021-01-26 17:16:13 -0300

    pm_menu: added /help in PM

--- handlers/pm_menu.py
+++ handlers/pm_menu.py
@@ -13,37 +13,49 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-import pyrogram
-import pyromod
 from config import prefix
 from pyrogram import Client, filters
 from pyrogram.types import Message, CallbackQuery
 from pyromod.helpers import ikb
 
 
-@Client.on_message(filters.command("start", prefix) & filters.group )
+@Client.on_message(filters.command("start", prefix) & filters.group)
 async def start_group(c: Client, m: Message):
     keyboard = ikb([
         [("Clique aqui para obter ajuda!", "http://t.me/PyKoroneBot?start", "url")]
     ])
     await m.reply_text(
-        f"Oi, eu sou o <b>Korone</b>, um bot interativo "
+        "Oi, eu sou o <b>Korone</b>, um bot interativo "
         "que adora participar de grupos!\n"
         "Você pode ver tudo que eu posso fazer clicando no botão abaixo...",
         reply_markup=keyboard,
     )
 
 
-@Client.on_message(filters.command("start", prefix) & filters.private )
+@Client.on_message(filters.command("start", prefix) & filters.private)
 async def start(c: Client, m: Message):
     keyboard = ikb([
         [("Ajuda", "help"),
-        ("Sobre", "about")],
+         ("Sobre", "about")],
         [("Grupo Off-Topic", "https://t.me/SpamTherapy", "url")]
     ])
     await m.reply_text(
-        f"Oi, eu sou o <b>Korone</b>, um bot interativo "
+        "Oi, eu sou o <b>Korone</b>, um bot interativo "
         "que adora participar de grupos!",
+        reply_markup=keyboard,
+    )
+
+
+@Client.on_message(filters.command("help", prefix) & filters.private)
+async def help_command(c: Client, m: Message):
+    keyboard = ikb([[("<- Voltar", "start_back", "callback_data")]])
+    await m.reply_text(
+        "<b>Aqui estão alguns dos meus comandos:</b>\n"
+        "• <code>/start</code>: Envi a mensagem inicial do bot.\n"
+        "• <code>/ping</code>: Envia o ping do bot.\n"
+        "• <code>/py</code>: Envia algumas informações técnicas do bot.\n"
+        "• <code>/copy</code>: O bot copia a mensagem que você responder com este comando.\n"
+        "• <code>/user</code>: Obtêm informações básicas de um usuário.",
         reply_markup=keyboard,
     )
 
@@ -53,7 +65,8 @@ async def help(c: Client, m: CallbackQuery):
     keyboard = ikb([[("<- Voltar", "start_back", "callback_data")]])
     await m.message.edit_text(
         "<b>Aqui estão alguns dos meus comandos:</b>\n"
-        "• <code>/start</code>: Envi a mensagem inicial do bot.\n"
+        "• <code>/start</code>: Envia a mensagem inicial do bot.\n"
+        "• <code>/help</code>: Envia a mensagem de ajuda do bot.\n",
         "• <code>/ping</code>: Envia o ping do bot.\n"
         "• <code>/py</code>: Envia algumas informações técnicas do bot.\n"
         "• <code>/copy</code>: O bot copia a mensagem que você responder com este comando.\n"
@@ -82,11 +95,11 @@ async def about(c: Client, m: CallbackQuery):
 async def start_back(c: Client, m: CallbackQuery):
     keyboard = ikb([
         [("Ajuda", "help"),
-        ("Sobre", "about")],
+         ("Sobre", "about")],
         [("Grupo Off-Topic", "https://t.me/SpamTherapy", "url")]
     ])
     await m.message.edit_text(
-        f"Oi, eu sou o <b>Korone</b>, um bot interativo "
+        "Oi, eu sou o <b>Korone</b>, um bot interativo "
         "que adora participar de grupos!",
         reply_markup=keyboard,
     )
