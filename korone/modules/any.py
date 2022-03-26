7d011bbbfdd61956114003689de2661e8bb7e13c
Hitalo <hitalo331@outlook.com>
2022-03-25 22:35:27 -0300

    any: removed inline help (will be re-written)

--- korone/modules/any.py
+++ korone/modules/any.py
@@ -6,12 +6,7 @@
 import html
 
 from pyrogram import filters
-from pyrogram.types import (
-    InlineQuery,
-    InlineQueryResultArticle,
-    InputTextMessageContent,
-    Message,
-)
+from pyrogram.types import Message
 
 from korone.database.chats import get_chat_by_id, register_chat_by_dict
 from korone.database.users import get_user_by_id, register_user_by_dict
@@ -56,42 +51,3 @@ async def thanks_for(c: Korone, m: Message):
             disable_notification=False,
             disable_web_page_preview=True,
         )
-
-
-@Korone.on_inline_query()
-async def inline_help(c: Korone, q: InlineQuery):
-    articles = [
-        InlineQueryResultArticle(
-            title="Informações",
-            input_message_content=InputTextMessageContent(
-                f"<b>Uso:</b> <code>@{c.me.username} user</code> - Exibe informações sobre você."
-            ),
-            description="Informações sobre você.",
-            thumb_url="https://piics.ml/amn/eduu/info.png",
-        ),
-        InlineQueryResultArticle(
-            title="Informações SpamWatch",
-            input_message_content=InputTextMessageContent(
-                f"<b>Uso:</b> <code>@{c.me.username} sw (id/username)</code> - Verifique se um usuário está banido no SpamWatch."
-            ),
-            description="Veja se um usuário está banido no SpamWatch.",
-            thumb_url="https://telegra.ph/file/1c18bdedaf01664dc0029.png",
-        ),
-        InlineQueryResultArticle(
-            title="Animes",
-            input_message_content=InputTextMessageContent(
-                f"<b>Uso:</b> <code>@{c.me.username} anime (pesquisa)</code> - Pesquise animes pelo inline."
-            ),
-            description="Pesquisa de animes com o Anilist.co.",
-            thumb_url="https://telegra.ph/file/c41e41a22dcf6479137d0.png",
-        ),
-        InlineQueryResultArticle(
-            title="Mangás",
-            input_message_content=InputTextMessageContent(
-                f"<b>Uso:</b> <code>@{c.me.username} manga (pesquisa)</code> - Pesquise mangás pelo inline."
-            ),
-            description="Pesquisa de mangás com o Anilist.co.",
-            thumb_url="https://telegra.ph/file/c41e41a22dcf6479137d0.png",
-        ),
-    ]
-    await q.answer(results=articles, cache_time=0)
