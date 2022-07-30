14d1e977a3df0007f6990e6ab1e60218ebac45f3
Hitalo <hitalo331@outlook.com>
2022-07-29 21:21:40 -0300

    pokemon: add disableable_dec and translations

--- korone/modules/pokemon.py
+++ korone/modules/pokemon.py
@@ -6,12 +6,16 @@ from pyrogram import filters
 from pyrogram.types import Message
 
 from korone.bot import Korone
+from korone.modules.utils.disable import disableable_dec
 from korone.modules.utils.images import pokemon_image_sync
+from korone.modules.utils.languages import get_strings_dec
 from korone.modules.utils.messages import get_args, get_command
 
 
 @Korone.on_message(filters.cmd(["pokemon", "backpokemon"]))
-async def poke_image(bot: Korone, message: Message):
+@disableable_dec("pokemon")
+@get_strings_dec("pokemon")
+async def poke_image(bot: Korone, message: Message, strings):
     args = get_args(message).split(" ")
     view = get_command(message, pure=True)
     if view == "backpokemon":
@@ -24,24 +28,22 @@ async def poke_image(bot: Korone, message: Message):
         r = await client.get("https://pokeapi.co/api/v2/pokemon/" + args[0])
 
         if r.status_code != 200:
-            await message.reply_text(f"<b>Error!</b> <code>{r.status_code}</code>")
+            await message.reply_text(strings["api_error"])
             return
 
         sprites = (r.json())["sprites"]
         if ptype not in sprites:
-            await message.reply_text(
-                f"<code>Error! Tipo '{args[1]}' não encontrado!</code>"
-            )
+            await message.reply_text(strings["type_not_found"].format(type=args[1]))
             return
 
         sprite_url = sprites[ptype]
         if not sprite_url:
-            await message.reply_text("Esse Pokémon não tem um sprite disponível!")
+            await message.reply_text(strings["no_sprite"])
             return
 
         r = await client.get(sprite_url)
         if r.status_code != 200:
-            await message.reply_text(f"<b>Error!</b> <code>{r.status_code}</code>")
+            await message.reply_text(strings["api_error"])
             return
 
     sprite_io = r.read()
@@ -50,25 +52,27 @@ async def poke_image(bot: Korone, message: Message):
 
 
 @Korone.on_message(filters.cmd("pokeitem"))
-async def poke_item_image(bot: Korone, message: Message):
+@disableable_dec("pokeitem")
+@get_strings_dec("pokemon")
+async def poke_item_image(bot: Korone, message: Message, strings):
     args = get_args(message)
 
     async with httpx.AsyncClient(http2=True) as client:
         r = await client.get(f"https://pokeapi.co/api/v2/item/{args}")
 
         if r.status_code != 200:
-            await message.reply_text(f"<b>Error!</b> <code>{r.status_code}</code>")
+            await message.reply_text(strings["api_error"])
             return
 
         sprites = (r.json())["sprites"]
         sprite_url = sprites["default"]
         if not sprite_url:
-            await message.reply_text("Esse item Pokémon não tem um sprite disponível!")
+            await message.reply_text(strings["unavailable_sprite"])
             return
 
         r = await client.get(sprite_url)
         if r.status_code != 200:
-            await message.reply_text(f"<b>Error!</b> <code>{r.status_code}</code>")
+            await message.reply_text(strings["api_error"])
             return
 
     sprite_io = r.read()
