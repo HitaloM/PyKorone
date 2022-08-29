46afbe1b94f1348a6b88fbcc843f505a4c8dab6c
Hitalo <hitalo331@outlook.com>
2022-08-29 19:29:24 -0300

    misc: added sed command

--- korone/modules/misc.py
+++ korone/modules/misc.py
@@ -1,7 +1,12 @@
 # SPDX-License-Identifier: BSD-3-Clause
 # Copyright (c) 2020-2022 Hitalo M. <https://github.com/HitaloM>
 
+import asyncio
+import html
+
 import httpx
+import regex
+from async_timeout import timeout
 from pyrogram import filters
 from pyrogram.types import Message
 
@@ -102,6 +107,55 @@ async def rpanda_photo(bot: Korone, message: Message, strings):
 
     rpanda = r.json()
     await message.reply_photo(rpanda["link"], caption="🐼")
+
+
+@Korone.on_message(filters.regex(r"^s/(.+)?/(.+)?(/.+)?") & filters.reply)
+@disableable_dec("sed")
+@get_strings_dec("misc")
+async def sed(bot: Korone, message: Message, strings):
+    exp = regex.split(r"(?<![^\\]\\)/", message.text)
+    pattern = exp[1]
+    replace_with = exp[2].replace(r"\/", "/")
+    flags = exp[3] if len(exp) > 3 else ""
+
+    count = 1
+    rflags = 0
+
+    if "g" in flags:
+        count = 0
+    if "i" in flags and "s" in flags:
+        rflags = regex.I | regex.S
+    elif "i" in flags:
+        rflags = regex.I
+    elif "s" in flags:
+        rflags = regex.S
+
+    text = message.reply_to_message.text or message.reply_to_message.caption
+
+    if not text:
+        return
+
+    try:
+        async with timeout(0.1):
+            res = regex.sub(
+                pattern,
+                replace_with,
+                text,
+                count=count,
+                flags=rflags,
+            )
+    except (asyncio.TimeoutError, TimeoutError):
+        await message.reply_text(
+            strings["regex_timeout"],
+        )
+        return
+    except regex.error as e:
+        await message.reply_text(
+            strings["regex_error"].format(error=e),
+        )
+        return
+
+    await message.reply_to_message.reply_text(html.escape(res))
 
 
 __help__ = True
