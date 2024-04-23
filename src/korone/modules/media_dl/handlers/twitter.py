66f90296e3af51cf4245b54ef21625ed28b1f059
Hitalo M. <hitalo331@outlook.com>
2024-04-23 00:39:13 -0300

    fix(media_dl): twitter single file upload

--- src/korone/modules/media_dl/handlers/twitter.py
+++ src/korone/modules/media_dl/handlers/twitter.py
@@ -5,6 +5,7 @@ import re
 from datetime import timedelta
 
 import aiohttp
+from hairydogm.keyboard import InlineKeyboardBuilder
 from hydrogram import Client
 from hydrogram.types import InputMediaPhoto, InputMediaVideo, Message
 from magic_filter import F
@@ -18,7 +19,8 @@ from korone.modules.utils.filters.magic import Magic
 class TwitterHandler(MessageHandler):
     def __init__(self):
         self.url_pattern = re.compile(r"https?://(?:www\.)?(twitter\.com|x\.com)/.+?/status/\d+")
-        self.media_pattern = re.compile(r"\.(mp4|jpg|jpeg|png)")
+        self.img_pattern = re.compile(r"\.(jpg|jpeg|png)")
+        self.vid_pattern = re.compile(r"\.(mp4|webm)")
 
     @staticmethod
     @cache(ttl=timedelta(hours=1))
@@ -35,6 +37,7 @@ class TwitterHandler(MessageHandler):
             "mediaURLs": data.get("mediaURLs", []),
             "text": data.get("text", ""),
             "user_name": data.get("user_name", ""),
+            "tweetURL": data.get("tweetURL", ""),
         }
 
     @router.message(
@@ -55,20 +58,32 @@ class TwitterHandler(MessageHandler):
         text = f"<b>{data["user_name"]}:</b>\n\n"
         text += data["text"]
 
-        if len(data["mediaURLs"]) == 1:
-            if re.search(r"\.mp4", data["mediaURLs"][0]):
-                await message.reply_video(data["mediaURLs"][0], caption=text)
+        keyboard = InlineKeyboardBuilder()
+        keyboard.button(text="Open in Twitter", url=data["tweetURL"])
+
+        media_urls = data["mediaURLs"]
+        if len(media_urls) == 1:
+            text = re.sub(r"https?://t\.co/\S*", "", text)
+            if isinstance(media_urls[0], InputMediaVideo):
+                await message.reply_video(
+                    media_urls[0], caption=text, reply_markup=keyboard.as_markup()
+                )
+            else:
+                await message.reply_photo(
+                    media_urls[0], caption=text, reply_markup=keyboard.as_markup()
+                )
             return
 
-        media_list = [
-            InputMediaVideo(media) if re.search(r"\.mp4", media) else InputMediaPhoto(media)
-            for media in data["mediaURLs"]
-            if re.search(self.media_pattern, media)
-        ]
+        media_list = []
+        for media in media_urls:
+            if re.search(self.img_pattern, media):
+                if re.search(self.vid_pattern, media):
+                    media_list.append(InputMediaVideo(media))
+                else:
+                    media_list.append(InputMediaPhoto(media))
 
         if not media_list:
             return
 
         media_list[-1].caption = text
-
         await message.reply_media_group(media=media_list)
