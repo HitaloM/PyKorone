bafcff0e341b4e4bc82946932643f47543fbf94d
Hitalo M. <hitalo331@outlook.com>
2024-04-24 22:43:37 -0300

    refactor(media_dl): improved instagram logic

--- src/korone/modules/media_dl/handlers/instagram.py
+++ src/korone/modules/media_dl/handlers/instagram.py
@@ -20,7 +20,7 @@ from korone.utils.i18n import gettext as _
 class InstagramHandler(MessageHandler):
     def __init__(self) -> None:
         self.url_pattern = re.compile(
-            r"(?:https?:\/\/)?(?:www\.)?instagram\.com\/(?:p|reels)\/([^/?#&]+).*"
+            r"((?:https?:\/\/)?(?:www\.)?instagram\.com\/(?:p|reels|reel)\/([^/?#&]+)).*"
         )
         self.image_pattern = re.compile(r"(.+\.jpg)")
         self.video_pattern = re.compile(r"(.+\.mp4)")
@@ -36,39 +36,47 @@ class InstagramHandler(MessageHandler):
 
     @router.message(
         Magic(
-            F.text.regexp(r"((?:https?:\/\/)?(?:www\.)?instagram\.com\/(?:p|reels)\/([^/?#&]+)).*")
+            F.text.regexp(
+                r"(?:https?:\/\/)?(?:www\.)?instagram\.com\/(?:p|reels|reel)\/([^/?#&]+).*"
+            )
         )
     )
     async def handle(self, client: Client, message: Message) -> None:
-        url = self.url_pattern.search(message.text)
-        if not url:
+        post_url = self.url_pattern.search(message.text)
+        if not post_url:
             return
 
-        post_id = url.group(1)
+        post_id = post_url.group(2)
+        if not post_id:
+            return
+        if not re.match(r"^[A-Za-z0-9\-_]+$", post_id):
+            return
+
         dd_url = f"https://ddinstagram.com/images/{post_id}/"
 
-        url_list = []
+        url_list: list[str] = []
         for i in range(1, 11):
-            data = await self.fetch_data(dd_url + str(i))
-            if data is None:
+            url = await self.fetch_data(dd_url + str(i))
+            if not url:
                 break
-            url_list.append(data)
+            url_list.append(url)
 
         if not url_list:
             return
 
-        keyboard = InlineKeyboardBuilder()
-        keyboard.button(text=_("Open in Instagram"), url=url.group())
-
         if len(url_list) == 1:
-            media = url_list[0]
-            if re.search(self.image_pattern, media):
-                await message.reply_photo(media, reply_markup=keyboard.as_markup())
-            elif re.search(self.video_pattern, media):
-                await message.reply_video(media, reply_markup=keyboard.as_markup())
+            url = url_list[0]
+
+            keyboard = InlineKeyboardBuilder()
+            keyboard.button(text=_("Open in Instagram"), url=post_url.group())
+
+            if re.search(self.image_pattern, url):
+                await message.reply_photo(url, reply_markup=keyboard.as_markup())
+            elif re.search(self.video_pattern, url):
+                await message.reply_video(url, reply_markup=keyboard.as_markup())
             return
 
-        media_list = []
+        media_list: list[InputMediaPhoto | InputMediaVideo] = []
         for media in url_list:
             if re.search(self.image_pattern, media):
                 media_list.append(InputMediaPhoto(media))
@@ -78,5 +86,5 @@ class InstagramHandler(MessageHandler):
         if not media_list:
             return
 
-        media_list[-1].caption = f"<a href='{url.group()}'>{_("Open in Instagram")}</a>"
+        media_list[-1].caption = f"<a href='{post_url.group()}'>{_("Open in Instagram")}</a>"
         await message.reply_media_group(media_list)
