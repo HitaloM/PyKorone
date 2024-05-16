eb1f235b8af8b0194285cf3140bab7ecd30e14f2
Hitalo M. <hitalo331@outlook.com>
2024-05-16 16:21:23 -0300

    feat(disabling): added disableable command

---
+++ src/korone/modules/disabling/handlers/list.py
@@ -0,0 +1,27 @@
+# SPDX-License-Identifier: BSD-3-Clause
+# Copyright (c) 2024 Hitalo M. <https://github.com/HitaloM>
+
+from hydrogram import Client
+from hydrogram.types import Message
+
+from korone.decorators import router
+from korone.handlers.abstract.message_handler import MessageHandler
+from korone.modules import COMMANDS
+from korone.modules.utils.filters import Command, IsAdmin
+from korone.utils.i18n import gettext as _
+
+
+class ListDisableableHandler(MessageHandler):
+    @staticmethod
+    @router.message(Command("disableable") & IsAdmin)
+    async def handle(client: Client, message: Message) -> None:
+        disableable = list(COMMANDS)
+        for command in list(disableable):
+            if "parent" in COMMANDS[command]:
+                disableable.remove(command)
+
+        text = _("The following commands can be disabled:\n")
+        for command in disableable:
+            text += f"- <code>{command}</code>\n"
+
+        await message.reply(text)
