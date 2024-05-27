33c11c9546fe7c8cbc8bd498758b5ad941b27cc2
Hitalo M. <hitalo331@outlook.com>
2024-05-26 22:37:50 -0300

    fix(decorators): using default locale for callback

--- src/korone/decorators/save_chats.py
+++ src/korone/decorators/save_chats.py
@@ -231,26 +231,26 @@ class ChatManager:
         @wraps(func)
         async def wrapper(*args, **kwargs):
             update: Message | CallbackQuery
-            user: User
 
             try:
                 update = args[2]
             except IndexError:
                 update = args[1]
 
-            if isinstance(update, CallbackQuery):
-                update = update.message
-                user = update.from_user
+            is_callback = isinstance(update, CallbackQuery)
+            message = update.message if is_callback else update
+            user: User = update.from_user if is_callback else message.from_user
+
+            if is_callback:
                 await self.save_from_user(user)
             else:
-                user = update.from_user
-                await self.handle_message(update)
+                await self.handle_message(message)
 
             locale = i18n.default_locale
-            if update.chat.type == ChatType.PRIVATE and user and not user.is_bot:
+            if message.chat.type == ChatType.PRIVATE and user and not user.is_bot:
                 locale = await self.get_locale(user)
-            elif update.chat.type in {ChatType.GROUP, ChatType.SUPERGROUP}:
-                locale = await self.get_locale(update.chat)
+            elif message.chat.type in {ChatType.GROUP, ChatType.SUPERGROUP}:
+                locale = await self.get_locale(message.chat)
 
             with i18n.context(), i18n.use_locale(locale):
                 return await func(*args, **kwargs)
