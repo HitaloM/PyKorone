bd7aca30becfbeca0ad64741492f7593541dfad9
Hitalo M. <hitalo331@outlook.com>
2024-06-15 13:25:36 -0300

    fix(media_dl): update Instagram GraphQL API

--- src/korone/modules/media_dl/utils/instagram/scraper.py
+++ src/korone/modules/media_dl/utils/instagram/scraper.py
@@ -18,13 +18,33 @@ from korone.utils.logging import log
 
 TIMEOUT: int = 20
 HEADERS: dict[str, str] = {
-    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,"
-    "image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
-    "Accept-Language": "en-US,en;q=0.9",
-    "Connection": "close",
-    "Sec-Fetch-Mode": "navigate",
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 "
-    "(KHTML, like Gecko) Chrome/118.0.0.0 Safari/537.36",
+    "accept": "*/*",
+    "accept-language": "en-US,en;q=0.9",
+    "content-type": "application/x-www-form-urlencoded",
+    "origin": "https://www.instagram.com",
+    "priority": "u=1, i",
+    "sec-ch-prefers-color-scheme": "dark",
+    "sec-ch-ua": '"Google Chrome";v="125", "Chromium";v="125", "Not.A/Brand";v="24"',
+    "sec-ch-ua-full-version-list": (
+        '"Google Chrome";v="125.0.6422.142", '
+        '"Chromium";v="125.0.6422.142", '
+        '"Not.A/Brand";v="24.0.0.0"'
+    ),
+    "sec-ch-ua-mobile": "?0",
+    "sec-ch-ua-model": "",
+    "sec-ch-ua-platform": '"macOS"',
+    "sec-ch-ua-platform-version": '"12.7.4"',
+    "sec-fetch-dest": "empty",
+    "sec-fetch-mode": "cors",
+    "sec-fetch-site": "same-origin",
+    "user-agent": (
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) "
+        "Chrome/125.0.0.0 Safari/537.36"
+    ),
+    "x-asbd-id": "129477",
+    "x-bloks-version-id": "e2004666934296f275a5c6b2c9477b63c80977c7cc0fd4b9867cb37e36092b68",
+    "x-fb-friendly-name": "PolarisPostActionLoadPostQueryQuery",
+    "x-ig-app-id": "936619743392459",
 }
 
 
@@ -217,8 +237,47 @@ async def _parse_gql_data(
 async def parse_gql_data(post_id: str) -> dict[str, Any] | None:
     url = f"https://www.instagram.com/p/{post_id}/"
     params = {
-        "query_hash": "b3055c01b4b222b8a47dc12b090e4e64",
-        "variables": f'{{"shortcode":"{post_id}"}}',
+        "av": "0",
+        "__d": "www",
+        "__user": "0",
+        "__a": "1",
+        "__req": "k",
+        "__hs": "19888.HYP:instagram_web_pkg.2.1..0.0",
+        "dpr": "2",
+        "__ccg": "UNKNOWN",
+        "__rev": "1014227545",
+        "__s": "trbjos:n8dn55:yev1rm",
+        "__hsi": "7380500578385702299",
+        "__dyn": "7xeUjG1mxu1syUbFp40NonwgU7SbzEdF8aUco2qwJw5ux609vCwjE1xoswaq0yE6ucw5Mx62G5UswoE"
+        "cE7O2l0Fwqo31w9a9wtUd8-U2zxe2GewGw9a362W2K0zK5o4q3y1Sx-0iS2Sq2-azo7u3C2u2J0bS1LwTwKG1pg2"
+        "fwxyo6O1FwlEcUed6goK2O4UrAwCAxW6Uf9EObzVU8U",
+        "__csr": "n2Yfg_5hcQAG5mPtfEzil8Wn-DpKGBXhdczlAhrK8uHBAGuKCJeCieLDyExenh68aQAKta8p8ShogKk"
+        "F5yaUBqCpF9XHmmhoBXyBKbQp0HCwDjqoOepV8Tzk8xeXqAGFTVoCciGaCgvGUtVU-u5Vp801nrEkO0rC58xw41g"
+        "0VW07ISyie2W1v7F0CwYwwwvEkw8K5cM0VC1dwdi0hCbc094w6MU1xE02lzw",
+        "__comet_req": "7",
+        "lsd": "AVoPBTXMX0Y",
+        "jazoest": "2882",
+        "__spin_r": "1014227545",
+        "__spin_b": "trunk",
+        "__spin_t": "1718406700",
+        "fb_api_caller_class": "RelayModern",
+        "fb_api_req_friendly_name": "PolarisPostActionLoadPostQueryQuery",
+        "variables": f'{
+            {
+                "shortcode": f"{post_id}",
+                "fetch_comment_count": "40",
+                "parent_comment_count": "24",
+                "child_comment_count": "3",
+                "fetch_like_count": "10",
+                "fetch_tagged_user_count": "null",
+                "fetch_preview_comment_count": "2",
+                "has_threaded_comments": "true",
+                "hoisted_comment_id": "null",
+                "hoisted_reply_id": "null",
+            }
+        }',
+        "server_timestamps": "true",
+        "doc_id": "25531498899829322",
     }
     proxies = ConfigManager.get("korone", "PROXIES")
 
@@ -272,6 +331,12 @@ def process_data(data: dict[str, Any] | None, post_id: str) -> InstaData:
         raise NotFoundError(msg)
 
     shortcode_media = data["shortcode_media"]
+    if not shortcode_media:
+        shortcode_media = data["xdt_shortcode_media"]
+        if not shortcode_media:
+            msg = f"No shortcode media found for post ID: {post_id}"
+            raise NotFoundError(msg)
+
     username = shortcode_media["owner"]["username"]
     caption_edges = shortcode_media["edge_media_to_caption"]["edges"]
     caption = caption_edges[0]["node"]["text"] if caption_edges else None
