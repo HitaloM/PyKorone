eb715bd3efd55a0f99b5ebca5ea1004747f5b101
Yan Horobtsov <Yan.Horobtsov@a-eberle.de>
2024-06-18 06:39:26 +0200

    Remove old /crash command in order of the new /test_crash of the new error module

--- sophie_bot/legacy_modules/owner_stuff.py
+++ sophie_bot/legacy_modules/owner_stuff.py
@@ -1,5 +1,3 @@
-# Copyright (C) 2018 - 2020 MrYacha. All rights reserved. Source code available under the AGPL.
-# Copyright (C) 2019 Aiogram
 import math
 import os
 
@@ -13,27 +11,6 @@ from sophie_bot.utils.i18n import gettext as _
 from .utils.language import get_strings_dec
 from .utils.register import register, REGISTRED_COMMANDS
 from sophie_bot.filters.user_status import IsOP
-
-
-#
-# This file is part of SophieBot.
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Affero General Public License as
-# published by the Free Software Foundation, either version 3 of the
-# License, or (at your option) any later version.
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU Affero General Public License for more details.
-# You should have received a copy of the GNU Affero General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
-
-@register(IsOP(True), cmds="crash")
-async def crash(message):
-    test = 2 / 0
-    print(test)
 
 
 @register(IsOP(True), cmds="event")
