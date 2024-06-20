d7a2d8c998bd2de4410d2be6c6dfcc6736fac6af
Yan Horobtsov <Yan.Horobtsov@a-eberle.de>
2024-06-20 23:10:55 +0200

    Add dev models

---
+++ sophie_bot/db/models/chat_connections.py
@@ -0,0 +1,29 @@
+from typing import Optional, Annotated
+
+from beanie import Document, Link, Indexed
+from pymongo import ASCENDING, IndexModel
+
+from sophie_bot.db.models import ChatModel
+
+
+class ChatConnectionModel(Document):
+    # Old IDs
+    user_id: int
+    chat_id: int
+
+    # New links
+    group: Optional[Link[ChatModel]]
+    user: Annotated[Optional[Link[ChatModel]], Indexed(unique=True)]
+
+    class Settings:
+        name = "connections"
+        indexes = [
+            IndexModel([
+                ("user", ASCENDING),
+                ("group", ASCENDING),
+            ], unique=True, name="user_group"),
+            IndexModel([
+                ("user_id", ASCENDING),
+                ("chat_id", ASCENDING),
+            ], unique=True, name="legacy_user_id_chat_id"),
+        ]
