6a3cb5bd8951f4bad8446d43182c6b567a04145d
Hitalo M. <hitalo331@outlook.com>
2024-06-22 19:19:54 -0300

    feat(modules): added 'stickers' module

---
+++ src/korone/modules/stickers/__init__.py
@@ -0,0 +1,17 @@
+# SPDX-License-Identifier: BSD-3-Clause
+# Copyright (c) 2024 Hitalo M. <https://github.com/HitaloM>
+
+from babel.support import LazyProxy
+
+from korone.utils.i18n import lazy_gettext as _
+
+
+class ModuleInfo:
+    name: LazyProxy = _("Stickers")
+    summary: LazyProxy = _(
+        "This module contains commands for handling stickers and sticker packs."
+    )
+    doc: LazyProxy = _(
+        "<b>Commands:</b>\n"
+        "- /getsticker: Reply to a sticker to get it as a file and its file ID."
+    )
