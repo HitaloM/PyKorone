3eaf177a81043009b2b429990c48e7d8130327e9
Hitalo M. <hitalo331@outlook.com>
2024-06-23 23:00:29 -0300

    doc(translator): better help text

--- src/korone/modules/translator/__init__.py
+++ src/korone/modules/translator/__init__.py
@@ -15,8 +15,13 @@ class ModuleInfo:
     )
     doc: LazyProxy = _(
         "<b>Commands:</b>\n"
-        "- /tr &lt;source&gt; &lt;target&gt; &lt;text&gt;: Translates text from the source "
-        "language to the target language.\n\n"
+        "- /tr &lt;source&gt;-&lt;target&gt; &lt;text&gt;: Translates text from the source "
+        "language to the target language. Can also be used as reply to a message."
+        "\n\n<b>Examples:</b>\n"
+        "- Translate 'Hello, world!' from English to Spanish:\n"
+        "-> <code>/tr en-es Hello, world!</code>\n\n"
+        "- Translate 'Hallo, Welt!' from German to English:\n"
+        "-> <code>/tr en Hallo, Welt!</code>\n\n"
         "<b>Notes:</b>\n"
         "<b>Supported Source Languages:</b>\n"
         "BG, CS, DA, DE, EL, EN, ES, ET, FI, FR, HU, ID, IT, JA, KO, LT, LV, NB, NL, PL, PT, RO, "
