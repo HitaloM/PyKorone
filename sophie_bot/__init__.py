e07c13f2e8f991700d4a97ee6c30e47552561d6c
Yan Horobtsov <Yan.Horobtsov@a-eberle.de>
2024-07-12 18:19:25 +0200

    Add event isolation

--- sophie_bot/__init__.py
+++ sophie_bot/__init__.py
@@ -4,6 +4,7 @@ from aiogram import Bot, Dispatcher
 from aiogram.client.default import DefaultBotProperties
 from aiogram.client.telegram import PRODUCTION, TelegramAPIServer
 from aiogram.fsm.storage.base import DefaultKeyBuilder
+from aiogram.fsm.storage.memory import SimpleEventIsolation
 from aiogram.fsm.storage.redis import RedisStorage
 from redis.asyncio import Redis
 
@@ -29,4 +30,4 @@ redis = Redis(
     single_connection_client=True,
 )
 storage = RedisStorage(redis=redis, key_builder=DefaultKeyBuilder(prefix=str(CONFIG.redis_db_fsm)))
-dp = Dispatcher(storage=storage)
+dp = Dispatcher(storage=storage, events_isolation=SimpleEventIsolation())
