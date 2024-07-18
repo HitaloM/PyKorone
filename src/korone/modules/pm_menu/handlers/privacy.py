4cb89f795029313c73a657b0ac3513bd8d311428
Hitalo M. <hitalo331@outlook.com>
2024-07-18 18:53:50 -0300

    fix(pm_menu): removed 'back' button from `/privacy`

--- src/korone/modules/pm_menu/handlers/privacy.py
+++ src/korone/modules/pm_menu/handlers/privacy.py
@@ -3,14 +3,12 @@
 
 from hairydogm.keyboard import InlineKeyboardBuilder
 from hydrogram import Client
-from hydrogram.enums import ChatType
-from hydrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
+from hydrogram.types import InlineKeyboardMarkup, Message
 
 from korone import constants
 from korone.decorators import router
 from korone.filters import Command
 from korone.handlers.abstract import MessageHandler
-from korone.modules.pm_menu.callback_data import PMMenuCallback
 from korone.utils.i18n import gettext as _
 
 
@@ -22,14 +20,6 @@ class PrivacyBaseHandler:
             text=_("Documentation"),
             url=f"{constants.DOCS_URL}/en/latest/privacy.html",
         )
-
-        if message.chat.type == ChatType.PRIVATE:
-            keyboard.row(
-                InlineKeyboardButton(
-                    text=_("🔙 Back"), callback_data=PMMenuCallback(menu="start").pack()
-                )
-            )
-
         return keyboard.as_markup()
 
     @staticmethod
