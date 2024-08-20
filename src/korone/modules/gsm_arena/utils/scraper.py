834d7dec25d5870788814a463f5c7d16561c9cb9
Hitalo M. <hitalo331@outlook.com>
2024-08-20 10:49:52 -0300

    feat(gsm_arena): added error handling

--- src/korone/modules/gsm_arena/utils/scraper.py
+++ src/korone/modules/gsm_arena/utils/scraper.py
@@ -3,12 +3,14 @@
 
 import urllib.parse
 from datetime import timedelta
+from typing import Any
 
 import httpx
 from lxml import html
 
 from korone import cache
 from korone.utils.i18n import gettext as _
+from korone.utils.logging import logger
 
 from .types import PhoneSearchResult
 
@@ -24,7 +26,7 @@ HEADERS = {
 }
 
 
-def get_data_from_specs(specs_data: dict, category: str, attributes: list) -> str:
+def get_data_from_specs(specs_data: dict, category: str, attributes: list[str]) -> str:
     details = specs_data.get("specs", {}).get(category, {})
     return "\n".join(details.get(attr, "") for attr in attributes)
 
@@ -93,14 +95,21 @@ def format_phone(phone: dict) -> str:
 
 @cache(ttl=timedelta(days=1))
 async def fetch_html(url: str) -> str:
-    async with httpx.AsyncClient(headers=HEADERS, http2=True) as session:
-        response = await session.get(url)
-        response.raise_for_status()
-        encoding = response.encoding or "utf-8"
-        return response.content.decode(encoding)
+    try:
+        async with httpx.AsyncClient(headers=HEADERS, http2=True) as session:
+            response = await session.get(url)
+            response.raise_for_status()
+            encoding = response.encoding or "utf-8"
+            return response.content.decode(encoding)
+    except httpx.HTTPStatusError as e:
+        logger.error("[GSM Arena] HTTP error occurred: %s", e)
+        raise
+    except httpx.RequestError as e:
+        logger.error("[GSM Arena] Request error occurred: %s", e)
+        raise
 
 
-def extract_specs(specs_tables: list) -> dict:
+def extract_specs(specs_tables: list[Any]) -> dict:
     return {
         "specs": {
             feature: {
@@ -116,42 +125,56 @@ def extract_specs(specs_tables: list) -> dict:
 
 
 async def search_phone(phone: str) -> list[PhoneSearchResult]:
-    phone_html_encoded = urllib.parse.quote_plus(phone)
-    search_url = f"https://m.gsmarena.com/results.php3?sQuickSearch=yes&sName={phone_html_encoded}"
-    html_content = await fetch_html(search_url)
-    tree = html.fromstring(html_content)
-    found_phones = tree.xpath("//div[@class='general-menu material-card']//ul//li")
-
-    return [
-        PhoneSearchResult(
-            name=phone_tag.xpath(".//img/@title")[0],
-            url=phone_tag.xpath(".//a/@href")[0],
+    try:
+        phone_html_encoded = urllib.parse.quote_plus(phone)
+        search_url = (
+            f"https://m.gsmarena.com/results.php3?sQuickSearch=yes&sName={phone_html_encoded}"
         )
-        for phone_tag in found_phones
-    ]
+        html_content = await fetch_html(search_url)
+        tree = html.fromstring(html_content)
+        found_phones = tree.xpath("//div[@class='general-menu material-card']//ul//li")
+
+        return [
+            PhoneSearchResult(
+                name=phone_tag.xpath(".//img/@title")[0],
+                url=phone_tag.xpath(".//a/@href")[0],
+            )
+            for phone_tag in found_phones
+        ]
+    except Exception as e:
+        logger.error("[GSM Arena] Error searching for phone: %s", e)
+        return []
 
 
 async def check_phone_details(url: str) -> dict[str, str]:
-    url = f"https://www.gsmarena.com/{url}"
-    html_content = await fetch_html(url)
-    tree = html.fromstring(html_content)
-    specs_tables = tree.xpath("//table[@cellspacing='0']")
+    try:
+        url = f"https://www.gsmarena.com/{url}"
+        html_content = await fetch_html(url)
+        tree = html.fromstring(html_content)
+        specs_tables = tree.xpath("//table[@cellspacing='0']")
 
-    phone_specs_temp = extract_specs(specs_tables)
+        phone_specs_temp = extract_specs(specs_tables)
 
-    meta_scripts = tree.xpath("//script[@language='javascript']")
-    if not meta_scripts:
-        msg = "No metadata scripts found on the page"
-        raise ValueError(msg)
+        meta_scripts = tree.xpath("//script[@language='javascript']")
+        if not meta_scripts:
+            msg = "No metadata scripts found on the page"
+            raise ValueError(msg)
 
-    meta = meta_scripts[0].text_content().splitlines()
-    name = extract_meta_data(meta, "ITEM_NAME")
-    picture = extract_meta_data(meta, "ITEM_IMAGE")
+        meta = meta_scripts[0].text_content().splitlines()
+        name = extract_meta_data(meta, "ITEM_NAME")
+        picture = extract_meta_data(meta, "ITEM_IMAGE")
 
-    phone_specs_temp.update({"name": name, "picture": picture, "url": url})
+        phone_specs_temp.update({"name": name, "picture": picture, "url": url})
 
-    return phone_specs_temp
+        return phone_specs_temp
+    except Exception as e:
+        logger.error("[GSM Arena] Error checking phone details: %s", e)
+        return {}
 
 
 def extract_meta_data(meta: list[str], key: str) -> str:
-    return next((line.split('"')[1] for line in meta if key in line), "")
+    try:
+        return next((line.split('"')[1] for line in meta if key in line), "")
+    except StopIteration:
+        logger.warning("[GSM Arena] Metadata key '%s' not found.", key)
+        return ""
