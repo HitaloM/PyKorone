f3a3875ca5486da59412a1a6dd1080e55ad5a5d1
Hitalo M. <hitalo331@outlook.com>
2024-08-23 17:01:00 -0300

    feat(filters): enhance IsSudo filter with type checks and logging
    - Ensure SUDOERS is a list of integers and raise TypeError if not
    - Add logging to monitor access attempts and errors
    - Verify SUDOERS list is loaded correctly and raise ValueError if not
    - Add async logging for authorized access attempts
    - Check if from_user is not None before accessing user_id
    - Update __call__ method to be async for non-blocking operations
    
    These changes improve the security and robustness of the IsSudo filter by ensuring proper configuration and providing detailed logs for access attempts.

--- src/korone/filters/sudo.py
+++ src/korone/filters/sudo.py
@@ -1,13 +1,25 @@
 # SPDX-License-Identifier: BSD-3-Clause
 # Copyright (c) 2024 Hitalo M. <https://github.com/HitaloM>
 
+from collections.abc import Generator
+
 from hydrogram import Client
 from hydrogram.filters import Filter
 from hydrogram.types import CallbackQuery, Message
 
 from korone.config import ConfigManager
+from korone.utils.logging import logger
 
 SUDOERS: list[int] = ConfigManager.get("korone", "SUDOERS")
+if not SUDOERS:
+    msg = "The SUDOERS list has not been loaded correctly. Check your configuration file."
+    logger.error(msg)
+    raise ValueError(msg)
+
+if not isinstance(SUDOERS, list) or not all(isinstance(i, int) for i in SUDOERS):
+    msg = "The SUDOERS list must be a list of integers. Check your configuration file."
+    logger.error(msg)
+    raise TypeError(msg)
 
 
 class IsSudo(Filter):
@@ -17,9 +29,25 @@ class IsSudo(Filter):
         self.client = client
         self.update = update
 
-    def __call__(self) -> bool:
+    async def __call__(self) -> bool:
         update = self.update
         is_callback = isinstance(update, CallbackQuery)
         message = update.message if is_callback else update
 
-        return False if message.from_user is None else message.from_user.id in SUDOERS
+        if message.from_user is None:
+            return False
+
+        user_id = message.from_user.id
+        chat_id = message.chat.id
+        if user_id in SUDOERS:
+            await logger.ainfo(
+                "[Filters/Sudo] Access allowed for user %s in %s.", user_id, chat_id
+            )
+            return True
+        await logger.awarning(
+            "[Filters/Sudo] Unauthorized access attempt by user %s in %s.", user_id, chat_id
+        )
+        return False
+
+    def __await__(self) -> Generator:
+        return self.__call__().__await__()  # type: ignore
