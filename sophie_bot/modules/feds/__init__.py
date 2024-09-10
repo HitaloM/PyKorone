b91e271490638600b5a802b216aaa8524a577a74
yacha <yacha@orangefox.tech>
2024-09-10 18:52:50 +0000

    Feds: migrate check_fbanned to middleware as it's own module to fix race-condition, additionally fix formatting issues

---
+++ sophie_bot/modules/feds/__init__.py
@@ -0,0 +1,9 @@
+from aiogram import Router
+
+from sophie_bot.modules.feds.middlewares.check_fban import FedBanMiddleware
+
+router = Router(name="info")
+
+
+def __pre_setup__():
+    router.message.outer_middleware(FedBanMiddleware())
--- sophie_bot/modules/legacy_modules/modules/feds.py
+++ sophie_bot/modules/legacy_modules/modules/feds.py
@@ -30,7 +30,6 @@ from contextlib import suppress
 from datetime import datetime, timedelta
 from typing import Optional
 
-import babel
 import ujson
 from aiogram import F, Router
 from aiogram.exceptions import (
@@ -50,7 +49,6 @@ from aiogram.types import (
 )
 from babel.dates import format_timedelta
 from pymongo import DeleteMany, InsertOne
-from stfu_tg import Template, UserLink
 
 from sophie_bot import bot, dp
 from sophie_bot.config import CONFIG
@@ -76,14 +74,12 @@ from sophie_bot.modules.legacy_modules.utils.user_details import (
     get_user_and_text,
     get_user_link,
     is_chat_creator,
-    is_user_admin,
 )
 from sophie_bot.services.db import db
 from sophie_bot.services.redis import redis
 from sophie_bot.services.telethon import tbot
 from sophie_bot.utils.cached import cached
 from sophie_bot.utils.i18n import lazy_gettext as l_
-from sophie_bot.utils.logger import log
 
 __module_name__ = l_("Federations")
 __module_emoji__ = "🏙"
@@ -1111,64 +1107,6 @@ async def importfbans_func(message: Message, fed, strings, document=None):
 async def import_state(message: Message, fed, state: FSMContext, **kwargs):
     await importfbans_func(message, fed, document=message.document)
     await state.clear()
-
-
-@register(router, only_groups=True)
-@chat_connection(only_groups=True)
-@get_strings_dec("feds")
-async def check_fbanned(message: Message, chat, strings):
-    if message.sender_chat:
-        # should be channel/anon
-        return
-
-    user_id = message.from_user.id
-    user_name = message.from_user.first_name
-    chat_id = chat["chat_id"]
-
-    log.debug("Enforcing fban check on {} in {}".format(user_id, chat_id))
-
-    if not (fed := await get_fed_f(message)):
-        return
-
-    elif await is_user_admin(chat_id, user_id):
-        return
-
-    feds_list = [fed["fed_id"]]
-
-    if "subscribed" in fed:
-        feds_list.extend(fed["subscribed"])
-
-    if ban := await db.fed_bans.find_one({"fed_id": {"$in": feds_list}, "user_id": user_id}):
-
-        # check whether banned fed_id is chat's fed id else
-        # user is banned in sub fed
-        if fed["fed_id"] == ban["fed_id"] and "origin_fed" not in ban:
-            doc = Template(
-                strings["automatic_ban"],
-                user=UserLink(user_id, user_name),
-                fed_name=html.escape(fed["fed_name"], False),
-            )
-        else:
-            s_fed = await get_fed_by_id(ban["fed_id"] if "origin_fed" not in ban else ban["origin_fed"])
-            if s_fed is None:
-                return
-
-            doc = Template(
-                strings["automatic_ban_sfed"],
-                user=UserLink(user_id, user_name),
-                fed_name=s_fed["fed_name"],
-            )
-
-        if "reason" in ban:
-            doc += Template(strings["automatic_ban_reason"], text=ban["reason"])
-
-        if not await ban_user(chat_id, user_id):
-            return
-
-        with suppress(TelegramBadRequest):
-            await message.reply(str(doc))
-
-        await db.fed_bans.update_one({"_id": ban["_id"]}, {"$addToSet": {"banned_chats": chat_id}})
 
 
 @register(router, cmds=["fcheck", "fbanstat"])
