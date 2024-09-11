22fb09a8750210de7768630c650b26c894813088
Hitalo M. <hitalo331@outlook.com>
2024-09-11 01:43:55 -0300

    refactor(filters): simplified the database logic

--- src/korone/modules/filters/database.py
+++ src/korone/modules/filters/database.py
@@ -2,13 +2,12 @@
 # Copyright (c) 2024 Hitalo M.
 
 import time
-from collections import defaultdict
 
 import orjson
 
 from korone.database.query import Query
 from korone.database.sqlite import SQLite3Connection
-from korone.database.table import Document, Documents, Table
+from korone.database.table import Document
 from korone.modules.filters.utils.types import Button, FilterFile, FilterModel, UserModel
 from korone.utils.caching import cache
 
@@ -34,115 +33,66 @@ async def save_filter(
         current_timestamp = int(time.time())
 
         existing_filters = await filters_table.query(Query().chat_id == chat_id)
-        filters_to_remove, filters_to_update = classify_filters(existing_filters, filter_names)
 
-        await remove_filters(filters_table, chat_id, filters_to_remove)
-        await update_filters(
-            filters_table,
-            Query(),
-            chat_id,
-            filters_to_update,
-            filter_names,
-            editor_id,
-            current_timestamp,
-        )
-        await insert_new_filter(
-            filters_table,
-            chat_id,
-            serialized_names,
-            filter_text,
-            content_type,
-            creator_id,
-            editor_id,
-            current_timestamp,
-            file_id,
-            serialized_buttons,
+        existing_filter = await filters_table.query(
+            (Query().chat_id == chat_id) & (Query().filter_names == serialized_names)
         )
 
+        if existing_filter:
+            existing_filter = existing_filter[0]
+            created_date = existing_filter["created_date"]
+            creator_id = existing_filter["creator_id"]
 
-def classify_filters(
-    existing_filters: Documents, new_filter_names: tuple[str, ...]
-) -> tuple[list[tuple[str, ...]], list[tuple[str, ...]]]:
-    classified = defaultdict(list)
-    new_filter_set = set(new_filter_names)
-
-    for existing_filter in existing_filters:
-        existing_filter_names = tuple(orjson.loads(existing_filter["filter_names"]))
-        existing_filter_set = set(existing_filter_names)
-
-        if existing_filter_set.issubset(new_filter_set):
-            classified["remove"].append(existing_filter_names)
-        elif new_filter_set.intersection(existing_filter_set):
-            classified["update"].append(existing_filter_names)
-
-    return classified["remove"], classified["update"]
-
-
-async def remove_filters(
-    filters_table: Table, chat_id: int, filters_to_remove: list[tuple[str, ...]]
-) -> None:
-    for filter_names in filters_to_remove:
-        await filters_table.delete(
-            (Query().chat_id == chat_id)
-            & (Query().filter_names == orjson.dumps(filter_names).decode())
-        )
-
-
-async def update_filters(
-    filters_table: Table,
-    query: Query,
-    chat_id: int,
-    filters_to_update: list[tuple[str, ...]],
-    new_filter_names: tuple[str, ...],
-    editor_id: int,
-    current_timestamp: int,
-) -> None:
-    for filter_names in filters_to_update:
-        if updated_filter_names := tuple(
-            name for name in filter_names if name not in new_filter_names
-        ):
             await filters_table.update(
                 Document(
-                    filter_names=orjson.dumps(updated_filter_names).decode(),
+                    filter_text=filter_text,
+                    content_type=content_type,
+                    created_date=created_date,
+                    creator_id=creator_id,
                     edited_date=current_timestamp,
                     editor_id=editor_id,
+                    file_id=file_id,
+                    buttons=serialized_buttons,
                 ),
-                (query.chat_id == chat_id)
-                & (query.filter_names == orjson.dumps(filter_names).decode()),
+                (Query().chat_id == chat_id) & (Query().filter_names == serialized_names),
             )
         else:
-            await filters_table.delete(
-                (query.chat_id == chat_id)
-                & (query.filter_names == orjson.dumps(filter_names).decode())
+            await filters_table.insert(
+                Document(
+                    chat_id=chat_id,
+                    filter_names=serialized_names,
+                    file_id=file_id,
+                    filter_text=filter_text,
+                    content_type=content_type,
+                    created_date=current_timestamp,
+                    creator_id=creator_id,
+                    edited_date=current_timestamp,
+                    editor_id=editor_id,
+                    buttons=serialized_buttons,
+                )
             )
 
-
-async def insert_new_filter(
-    filters_table: Table,
-    chat_id: int,
-    serialized_names: str,
-    filter_text: str,
-    content_type: str,
-    creator_id: int,
-    editor_id: int,
-    current_timestamp: int,
-    file_id: str | None,
-    serialized_buttons: str,
-) -> None:
-    await filters_table.insert(
-        Document(
-            chat_id=chat_id,
-            filter_names=serialized_names,
-            file_id=file_id,
-            filter_text=filter_text,
-            content_type=content_type,
-            created_date=current_timestamp,
-            creator_id=creator_id,
-            edited_date=current_timestamp,
-            editor_id=editor_id,
-            buttons=serialized_buttons,
-        )
-    )
+        for existing_filter in existing_filters:
+            existing_filter_names = tuple(orjson.loads(existing_filter["filter_names"]))
+            if set(existing_filter_names).intersection(set(filter_names)):
+                updated_filter_names = tuple(
+                    name for name in existing_filter_names if name not in filter_names
+                )
+                if updated_filter_names:
+                    await filters_table.update(
+                        Document(
+                            filter_names=orjson.dumps(updated_filter_names).decode(),
+                            edited_date=current_timestamp,
+                            editor_id=editor_id,
+                        ),
+                        (Query().chat_id == chat_id)
+                        & (Query().filter_names == orjson.dumps(existing_filter_names).decode()),
+                    )
+                elif existing_filter_names != filter_names:
+                    await filters_table.delete(
+                        (Query().chat_id == chat_id)
+                        & (Query().filter_names == orjson.dumps(existing_filter_names).decode())
+                    )
 
 
 async def list_filters(chat_id: int) -> list[FilterModel]:
