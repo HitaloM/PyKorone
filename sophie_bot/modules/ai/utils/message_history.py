648a577c23e9ef45c09c2c6cdf7ced9c5fc885f5
yacha <yacha@orangefox.tech>
2024-10-06 15:47:55 +0200

    Make commit

--- sophie_bot/modules/ai/utils/message_history.py
+++ sophie_bot/modules/ai/utils/message_history.py
@@ -3,7 +3,6 @@ from base64 import b64encode
 from typing import BinaryIO, Optional
 
 from aiogram.types import Message
-from devtools import debug
 from openai.types.chat import (
     ChatCompletionContentPartImageParam,
     ChatCompletionContentPartParam,
