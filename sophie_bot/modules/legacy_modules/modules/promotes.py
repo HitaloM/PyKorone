1e42dd149379d2578ded60387d059356d087d90c
yacha <yacha@orangefox.tech>
2024-10-08 13:55:38 +0000

    Promotes: Use star as the module emoji

--- sophie_bot/modules/legacy_modules/modules/promotes.py
+++ sophie_bot/modules/legacy_modules/modules/promotes.py
@@ -36,7 +36,7 @@ from sophie_bot.services.telethon import tbot
 from sophie_bot.utils.i18n import lazy_gettext as l_
 
 __module_name__ = l_("Promotes")
-__module_emoji__ = "⛔️"
+__module_emoji__ = "⭐️"
 
 router = Router(name="promotes")
 
