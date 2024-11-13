cbe9053fec9a8b17e27a3380dec2d7d385b09dab
yacha <yacha@orangefox.tech>
2024-11-13 21:37:21 +0100

    Fixup needs_translation For some unknown reason, it has to be the bottom in the response model, or else AI always returns False
    I guess it's just magic

--- sophie_bot/modules/ai/json_schemas/translate.py
+++ sophie_bot/modules/ai/json_schemas/translate.py
@@ -4,10 +4,8 @@ from pydantic import BaseModel, Field
 
 
 class AITranslateResponseSchema(BaseModel):
+    needs_translation: bool = Field(description="Whatever the provided text needs translation")
     translated_text: str = Field(description="Translated text, word to word with original")
-    needs_translation: bool = Field(
-        description="Whatever the provided text needs translation (True) or already in the required language (False)"
-    )
     origin_language_name: str = Field(description="Origin language of the text")
     origin_language_emoji: str = Field(description="Flag of the origin language")
     translation_explanations: Optional[str] = Field(
