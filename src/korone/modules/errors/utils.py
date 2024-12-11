91c865400a5e480ba0a036109d2c144535849465
Hitalo M. <hitalo331@outlook.com>
2024-12-11 16:58:05 -0300

    fix(errors): add MessageIdInvalid to ignored exceptions list

--- src/korone/modules/errors/utils.py
+++ src/korone/modules/errors/utils.py
@@ -1,6 +1,11 @@
 # SPDX-License-Identifier: BSD-3-Clause
 # Copyright (c) 2024 Hitalo M. <https://github.com/HitaloM>
 
-from hydrogram.errors import ChannelPrivate, ChatWriteForbidden, FloodWait
+from hydrogram.errors import ChannelPrivate, ChatWriteForbidden, FloodWait, MessageIdInvalid
 
-IGNORED_EXCEPTIONS: tuple[type[Exception], ...] = (FloodWait, ChatWriteForbidden, ChannelPrivate)
+IGNORED_EXCEPTIONS: tuple[type[Exception], ...] = (
+    FloodWait,
+    ChatWriteForbidden,
+    ChannelPrivate,
+    MessageIdInvalid,
+)
