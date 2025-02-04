b988b49fa11253e0c1771cd5a99d63ee525e3483
Yacha <yacha@orangefox.tech>
2025-02-04 16:30:46 +0100

    EnforceFiltersMiddleware: Remove unused `_is_user_admin` method from enforce_middleware
    The `_is_user_admin` method was redundant and no longer utilized in the code. Its removal improves code clarity and eliminates unnecessary complexity.

--- sophie_bot/modules/filters/enforce_middleware.py
+++ sophie_bot/modules/filters/enforce_middleware.py
@@ -27,12 +27,6 @@ class EnforceFiltersMiddleware(BaseMiddleware):
     def _get_all_cmds() -> tuple[str, ...]:
         return get_all_cmds_raw()
 
-    @staticmethod
-    async def _is_user_admin(message: Message) -> bool:
-        if not message.from_user:
-            return False
-        return await is_user_admin(message.chat.id, message.from_user.id)
-
     async def _is_to_drop(self, message: Message, state: Optional[FSMContext]) -> bool:
         sender: Optional[User | Chat] = message.sender_chat or message.from_user
 
