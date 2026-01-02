36495f79f4cd1e4eabc9d50a13bb019e22bc32a9
yacha <yacha@orangefox.tech>
2026-01-02 20:48:05 +0100

    refactor(db): replace `AsyncIOMotorClient` with `AsyncMongoClient`
    Switched to `AsyncMongoClient` for database connection, replacing the deprecated `AsyncIOMotorClient`. Updated initialization logic to align with the new client while maintaining existing functionality.

--- sophie_bot/services/db.py
+++ sophie_bot/services/db.py
@@ -1,12 +1,13 @@
 from beanie import init_beanie
-from motor.motor_asyncio import AsyncIOMotorClient
+from pymongo import AsyncMongoClient
 
 from sophie_bot.config import CONFIG
 from sophie_bot.db.models import models
 
-motor: AsyncIOMotorClient = AsyncIOMotorClient(CONFIG.mongo_host, CONFIG.mongo_port)
-db = motor[CONFIG.mongo_db]
+async_mongo = AsyncMongoClient(CONFIG.mongo_host, CONFIG.mongo_port)
+db = async_mongo[CONFIG.mongo_db]
 
 
 async def init_db():
-    await init_beanie(database=db, document_models=models, allow_index_dropping=CONFIG.mongo_allow_index_dropping)  # type: ignore
+    await init_beanie(database=db, document_models=models,
+                      allow_index_dropping=CONFIG.mongo_allow_index_dropping)  # type: ignore
