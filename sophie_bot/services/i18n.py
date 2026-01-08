70d02caff8d1412542aa63de914423b6a6737f2a
yacha <yacha@orangefox.tech>
2026-01-08 19:29:19 +0100

    chore(locales): update translation file references
    Updated line references in `sophie.pot` and `bot.pot` to align with recent code changes. Adjusted file paths for accurate localization mapping and added initialization for locale management in `i18n.py`.

--- sophie_bot/services/i18n.py
+++ sophie_bot/services/i18n.py
@@ -2,3 +2,4 @@ from sophie_bot.config import CONFIG
 from sophie_bot.utils.i18n import I18nNew
 
 i18n = I18nNew(path="locales", domain="sophie", default_locale=CONFIG.default_locale)
+i18n.set_current(i18n)
