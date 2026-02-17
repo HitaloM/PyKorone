9cb8c50fa7dcf73d7816a1926d7088fea9a87f39
Hitalo M. <hitalo331@outlook.com>
2026-02-17 15:31:45 -0300

    fix(language): remove unused export for chat language

--- src/korone/modules/language/__init__.py
+++ src/korone/modules/language/__init__.py
@@ -6,7 +6,6 @@ from korone.utils.i18n import lazy_gettext as l_
 
 from .callbacks import LangMenuCallback as LangMenuCallback
 from .callbacks import SetLangCallback as SetLangCallback
-from .export import export_chat_language
 from .handlers.apply import ApplyLanguageHandler
 from .handlers.info import LanguageInfoCallbackHandler, LanguageInfoHandler
 from .handlers.select import LanguageSelectCallbackHandler, LanguageSelectHandler, LanguageSelectPMHandler
@@ -18,8 +17,6 @@ __module_name__ = l_("Language")
 __module_emoji__ = "🌎"
 __module_description__ = l_("Language and localization settings")
 __module_info__ = LazyProxy(lambda: Doc(l_("Allows users to set their preferred language for bot interactions.")))
-
-__export__ = export_chat_language
 
 __handlers__ = (
     LanguageInfoHandler,
