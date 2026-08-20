7f62a7d7093de4a2c3cf224b29d75c946415b14b
Hitalo M. <hitalo331@outlook.com>
2026-08-20 20:27:47 -0300

    refactor(stats): format database size with ByteSize

--- src/korone/modules/op/handlers/stats.py
+++ src/korone/modules/op/handlers/stats.py
@@ -1,8 +1,8 @@
-import math
 from typing import TYPE_CHECKING
 
 from aiogram import flags
 from aiogram.filters import Command
+from pydantic import ByteSize
 
 from korone import aredis
 from korone.db.session import get_postgres_stats
@@ -15,16 +15,6 @@ if TYPE_CHECKING:
     from aiogram.dispatcher.event.handler import CallbackType
 
 
-def convert_size(size_bytes: int) -> str:
-    if size_bytes == 0:
-        return "0B"
-    size_name = ("B", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB")
-    i = math.floor(math.log(size_bytes, 1024))
-    p = math.pow(1024, i)
-    s = round(size_bytes / p, 2)
-    return f"{s} {size_name[i]}"
-
-
 async def get_system_stats() -> Doc:
     doc = Doc()
 
@@ -32,7 +22,8 @@ async def get_system_stats() -> Doc:
 
     local_db = await get_postgres_stats()
     technical_section += KeyValue(
-        "Database size", Template("{db_size}", db_size=Code(convert_size(local_db["db_size"])))
+        "Database size",
+        Template("{db_size}", db_size=Code(ByteSize(local_db["db_size"]).human_readable(decimal=False))),
     )
 
     technical_section += KeyValue("Redis keys", Code(await aredis.dbsize()))
